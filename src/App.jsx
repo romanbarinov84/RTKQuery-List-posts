dca3f27d5df5f60813ab1c019ae3be315d4316ad
Roman <roman-meinl@ukr.net>
2025-06-02 15:11:56 -0200

    create function allposts and oneposts

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import { postsApi } from "./services/postsApi";
 import { useGetPostsByIdQuery,useGetPostsQuery,useCreatedPostsMutation,useUpdatedPostsMutation,useDeletedPostsMutation } from "./services/postsApi";
 
 
 
 function App() {
+  const [postId , setPostId] = useState(1);
  const {data:posts, isLoading , error} = useGetPostsQuery();
+ const {data:post, isLoad: isLoadingPost , error:Errorpost} = useGetPostsByIdQuery(postId);
+
+  if(isLoading || isLoadingPost) return <div><h1>Загрузка данных</h1></div>
+  if(error || Errorpost) return <div><p>Ошибка : {error.status}</p></div>
 
 
+  const handleChange = (event) => {
+    setPostId(Number(event.target.value))
+    
+  }
 
   return (
     <>
     <div style={{ padding: "2rem",border:"5px solid green",marginLeft:"50px",textAlign:"center",background:"lightgray",boxShadow:"3px 4px 4px 2px #333" }}>
       <h1>Posts from rtk query</h1>
+      <input type="number" value={postId} onChange={handleChange} min={1}/>
+      <h2>Один пост</h2>
+       {post && <div style={{padding:"10px",border:"2px solid black",background:"wheat"}}>
+          <strong style={{color:"cornflowerblue",fontSize:"30px"}}>{post.title}</strong>
+          <p>{post.body}</p>
+          </div>}
+
+
+
+      <h2>все посты</h2>
       {posts && <ul style={{border:"3px solid #333",padding:"10px",background:"lightblue"}}>
         {
           posts.map((post) => (
@@ -24,6 +44,9 @@ function App() {
             </li>
           ))
         }</ul>}
+
+       
+        
       </div>;
     </>
   )
